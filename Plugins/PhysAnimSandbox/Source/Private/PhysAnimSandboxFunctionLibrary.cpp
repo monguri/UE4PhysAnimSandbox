cda4ae8f4f04834ae740f3ac719437a7ba7e1e1a
monguri <mongry@gmail.com>
2020-11-22 19:43:37 +0900

    スケルタルメッシュアセットを作るところまでもっていった。 RenderDataがないので作ったアセットをクリックしにいくとサムネイルを作ろうとしてクラッシュする

--- Plugins/PhysAnimSandbox/Source/Private/PhysAnimSandboxFunctionLibrary.cpp
+++ Plugins/PhysAnimSandbox/Source/Private/PhysAnimSandboxFunctionLibrary.cpp
@@ -1,8 +1,21 @@
 #include "PhysAnimSandboxFunctionLibrary.h"
+#include "Engine/SkeletalMesh.h"
 
 bool UPhysAnimSandboxFunctionLibrary::CreateSkeletalMesh()
 {
-	UE_LOG(LogTemp, Log, TEXT("CreateSkeletalMesh called."));
+	UPackage* Package = CreatePackage(nullptr, TEXT("/Game/NewSkeletalMesh"));
+	if(!ensure(Package))
+	{
+		return false;
+	}
+
+	USkeletalMesh* SkeletalMesh = NewObject<USkeletalMesh>(Package, FName("NewSkeletalMesh"), EObjectFlags::RF_Public | EObjectFlags::RF_Standalone | EObjectFlags::RF_Transactional);
+	if(!ensure(SkeletalMesh))
+	{
+		return false;
+	}
+
+	SkeletalMesh->MarkPackageDirty();
 	return true;
 }
 
