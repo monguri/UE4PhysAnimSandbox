463b7c46fdc7946befd7e02e5568aef0ceaf625e
monguri <mongry@gmail.com>
2020-11-23 17:48:59 +0900

    Boxを作ってみる。とりあえず2面作ったところまで

--- Plugins/PhysAnimSandbox/Source/Private/PhysAnimSandboxFunctionLibrary.cpp
+++ Plugins/PhysAnimSandbox/Source/Private/PhysAnimSandboxFunctionLibrary.cpp
@@ -23,6 +23,9 @@ bool UPhysAnimSandboxFunctionLibrary::CreateSkeletalMesh()
 	}
 
 	FSkeletalMeshImportData SkeletalMeshData;
+
+#if 0
+	// 2 bone 1 triangle
 	{
 		SkeletalMeshData.Points.Emplace(-10.0f + 50.0f, 10.0f, 0.0f);
 		SkeletalMeshData.Points.Emplace(10.0f + 50.0f, 10.0f, 0.0f);
@@ -68,7 +71,7 @@ bool UPhysAnimSandboxFunctionLibrary::CreateSkeletalMesh()
 		B0.Name = FString("Root");
 		B0.Flags = 0x02; //TODO
 		B0.NumChildren = 1;
-		B0.ParentIndex = INDEX_NONE; //TODO
+		B0.ParentIndex = INDEX_NONE;
 		B0.BonePos = J0;
 		B1.Name = FString("Child");
 		B1.Flags = 0x02; //TODO
@@ -108,6 +111,200 @@ bool UPhysAnimSandboxFunctionLibrary::CreateSkeletalMesh()
 		SkeletalMeshData.bUseT0AsRefPose = false; // こんなのあったんだな。クロスの初期化に使えそう
 		SkeletalMeshData.bDiffPose = false; // こんなのあったんだな。クロスの初期化に使えそう
 	}
+#else
+	// 9 bone 8 box
+	{
+		SkeletalMeshData.Points.Emplace(-10.0f + 50.0f, 10.0f, 10.0f);
+		SkeletalMeshData.Points.Emplace(10.0f + 50.0f, 10.0f, 10.0f);
+		SkeletalMeshData.Points.Emplace(-10.0f + 50.0f, -10.0f, 10.0f);
+		SkeletalMeshData.Points.Emplace(10.0f + 50.0f, -10.0f, 10.0f);
+
+		SkeletalMeshData.Points.Emplace(-10.0f + 50.0f, 10.0f, -10.0f);
+		SkeletalMeshData.Points.Emplace(10.0f + 50.0f, 10.0f, -10.0f);
+		SkeletalMeshData.Points.Emplace(-10.0f + 50.0f, -10.0f, -10.0f);
+		SkeletalMeshData.Points.Emplace(10.0f + 50.0f, -10.0f, -10.0f);
+
+		const float OneThird = 1.0f / 3.0f;
+		const float OneFourth = 0.25f;
+
+		// Trigngle T0
+		SkeletalMeshImportData::FVertex V00, V01, V02;
+		V00.VertexIndex = 0;
+		V00.UVs[0] = FVector2D(OneThird, 0.0f);
+		V00.MatIndex = 0;
+		V01.VertexIndex = 1;
+		V01.UVs[0] = FVector2D(OneThird * 2.0f, 0.0f);
+		V01.MatIndex = 0;
+		V02.VertexIndex = 2;
+		V02.UVs[0] = FVector2D(OneThird, OneFourth);
+		V02.MatIndex = 0;
+
+		SkeletalMeshData.Wedges.Add(V00);
+		SkeletalMeshData.Wedges.Add(V01);
+		SkeletalMeshData.Wedges.Add(V02);
+
+		SkeletalMeshImportData::FTriangle T0;
+		T0.WedgeIndex[0] = 0;
+		T0.WedgeIndex[1] = 1;
+		T0.WedgeIndex[2] = 2;
+		T0.MatIndex = 0;
+		T0.SmoothingGroups = 0;
+
+		SkeletalMeshData.Faces.Add(T0);
+
+		// Trigngle T1
+		SkeletalMeshImportData::FVertex V10, V11, V12;
+		V10.VertexIndex = 3;
+		V10.UVs[0] = FVector2D(OneThird * 2.0f, OneFourth);
+		V10.MatIndex = 0;
+		V11.VertexIndex = 2;
+		V11.UVs[0] = FVector2D(OneThird, OneFourth);
+		V11.MatIndex = 0;
+		V12.VertexIndex = 1;
+		V12.UVs[0] = FVector2D(OneThird * 2.0f, 0.0f);
+		V12.MatIndex = 0;
+
+		SkeletalMeshData.Wedges.Add(V10);
+		SkeletalMeshData.Wedges.Add(V11);
+		SkeletalMeshData.Wedges.Add(V12);
+
+		SkeletalMeshImportData::FTriangle T1;
+		T1.WedgeIndex[0] = 3;
+		T1.WedgeIndex[1] = 4;
+		T1.WedgeIndex[2] = 5;
+		T1.MatIndex = 0;
+		T1.SmoothingGroups = 0;
+
+		SkeletalMeshData.Faces.Add(T1);
+
+		// Trigngle T2
+		SkeletalMeshImportData::FVertex V20, V21, V22;
+		V20.VertexIndex = 2;
+		V20.UVs[0] = FVector2D(OneThird, OneFourth);
+		V20.MatIndex = 0;
+		V21.VertexIndex = 3;
+		V21.UVs[0] = FVector2D(OneThird * 2.0f, OneFourth);
+		V21.MatIndex = 0;
+		V22.VertexIndex = 6;
+		V22.UVs[0] = FVector2D(OneThird, OneFourth * 2.0f);
+		V22.MatIndex = 0;
+
+		SkeletalMeshData.Wedges.Add(V20);
+		SkeletalMeshData.Wedges.Add(V21);
+		SkeletalMeshData.Wedges.Add(V22);
+
+		SkeletalMeshImportData::FTriangle T2;
+		T2.WedgeIndex[0] = 6;
+		T2.WedgeIndex[1] = 7;
+		T2.WedgeIndex[2] = 8;
+		T2.MatIndex = 0;
+		T2.SmoothingGroups = 0;
+
+		SkeletalMeshData.Faces.Add(T2);
+
+		// Trigngle T3
+		SkeletalMeshImportData::FVertex V30, V31, V32;
+		V30.VertexIndex = 7;
+		V30.UVs[0] = FVector2D(OneThird * 2.0f, OneFourth * 2.0f);
+		V30.MatIndex = 0;
+		V31.VertexIndex = 6;
+		V31.UVs[0] = FVector2D(OneThird, OneFourth * 2.0f);
+		V31.MatIndex = 0;
+		V32.VertexIndex = 3;
+		V32.UVs[0] = FVector2D(OneThird * 2.0f, OneFourth);
+		V32.MatIndex = 0;
+
+		SkeletalMeshData.Wedges.Add(V30);
+		SkeletalMeshData.Wedges.Add(V31);
+		SkeletalMeshData.Wedges.Add(V32);
+
+		SkeletalMeshImportData::FTriangle T3;
+		T3.WedgeIndex[0] = 9;
+		T3.WedgeIndex[1] = 10;
+		T3.WedgeIndex[2] = 11;
+		T3.MatIndex = 0;
+		T3.SmoothingGroups = 0;
+
+		SkeletalMeshData.Faces.Add(T3);
+
+
+		SkeletalMeshImportData::FJointPos J0, J1;
+		J0.Transform = FTransform::Identity;
+		J0.Length = 0.0f; // TODO
+		J0.XSize = 5.0f; // TODO
+		J0.YSize = 5.0f; // TODO
+		J0.ZSize = 5.0f; // TODO
+		J1.Transform = FTransform(FVector(50.0f, 0.0f, 0.0f));
+		J1.Length = 0.0f; // TODO
+		J1.XSize = 5.0f; // TODO
+		J1.YSize = 5.0f; // TODO
+		J1.ZSize = 5.0f; // TODO
+
+		SkeletalMeshImportData::FBone B0, B1;
+		B0.Name = FString("Root");
+		B0.Flags = 0x02; //TODO
+		B0.NumChildren = 1;
+		B0.ParentIndex = INDEX_NONE;
+		B0.BonePos = J0;
+		B1.Name = FString("Child");
+		B1.Flags = 0x02; //TODO
+		B1.NumChildren = 0;
+		B1.ParentIndex = 0;
+		B1.BonePos = J1;
+
+		SkeletalMeshData.RefBonesBinary.Add(B0);
+		SkeletalMeshData.RefBonesBinary.Add(B1);
+
+		SkeletalMeshImportData::FRawBoneInfluence I0, I1, I2, I3, I4, I5, I6, I7;
+		I0.Weight = 1.0f;
+		I0.VertexIndex = 0;
+		I0.BoneIndex = 1;
+		I1.Weight = 1.0f;
+		I1.VertexIndex = 1;
+		I1.BoneIndex = 1;
+		I2.Weight = 1.0f;
+		I2.VertexIndex = 2;
+		I2.BoneIndex = 1;
+		I3.Weight = 1.0f;
+		I3.VertexIndex = 3;
+		I3.BoneIndex = 1;
+		I4.Weight = 1.0f;
+		I4.VertexIndex = 4;
+		I4.BoneIndex = 1;
+		I5.Weight = 1.0f;
+		I5.VertexIndex = 5;
+		I5.BoneIndex = 1;
+		I6.Weight = 1.0f;
+		I6.VertexIndex = 6;
+		I6.BoneIndex = 1;
+		I7.Weight = 1.0f;
+		I7.VertexIndex = 7;
+		I7.BoneIndex = 1;
+
+		SkeletalMeshData.Influences.Add(I0);
+		SkeletalMeshData.Influences.Add(I1);
+		SkeletalMeshData.Influences.Add(I2);
+		SkeletalMeshData.Influences.Add(I3);
+		SkeletalMeshData.Influences.Add(I4);
+		SkeletalMeshData.Influences.Add(I5);
+		SkeletalMeshData.Influences.Add(I6);
+		SkeletalMeshData.Influences.Add(I7);
+
+		SkeletalMeshData.PointToRawMap.AddUninitialized(SkeletalMeshData.Points.Num());
+		for (int32 PointIdx = 0; PointIdx < SkeletalMeshData.Points.Num(); PointIdx++)
+		{
+			SkeletalMeshData.PointToRawMap[PointIdx] = PointIdx;
+		}
+
+		SkeletalMeshData.NumTexCoords = 1;
+		SkeletalMeshData.MaxMaterialIndex = 1; // TODO
+		SkeletalMeshData.bHasVertexColors = false;
+		SkeletalMeshData.bHasNormals = false;
+		SkeletalMeshData.bHasTangents = false;
+		SkeletalMeshData.bUseT0AsRefPose = false; // こんなのあったんだな。クロスの初期化に使えそう
+		SkeletalMeshData.bDiffPose = false; // こんなのあったんだな。クロスの初期化に使えそう
+	}
+#endif
 
 	FBox BoundingBox(SkeletalMeshData.Points.GetData(), SkeletalMeshData.Points.Num());
 
