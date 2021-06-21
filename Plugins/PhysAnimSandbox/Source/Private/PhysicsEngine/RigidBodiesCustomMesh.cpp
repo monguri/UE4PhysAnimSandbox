067e3f1c20f5700ec6f3fb9b47bbeba859d80c9e
monguri <mongry@gmail.com>
2021-06-21 23:01:05 +0900

    Boxのコリジョンシェイプデータに必要な頂点配列とエッジ配列を定義した

--- Plugins/PhysAnimSandbox/Source/Private/PhysicsEngine/RigidBodiesCustomMesh.cpp
+++ Plugins/PhysAnimSandbox/Source/Private/PhysicsEngine/RigidBodiesCustomMesh.cpp
@@ -13,6 +13,41 @@ ARigidBodiesCustomMesh::ARigidBodiesCustomMesh()
 void ARigidBodiesCustomMesh::BeginPlay()
 {
 	Super::BeginPlay();
+
+	static TArray<FVector> BoxVertices = 
+	{
+		FVector(-0.5, -0.5, -0.5),
+		FVector(+0.5, -0.5, -0.5),
+		FVector(-0.5, +0.5, -0.5),
+		FVector(+0.5, +0.5, -0.5),
+		FVector(-0.5, -0.5, +0.5),
+		FVector(+0.5, -0.5, +0.5),
+		FVector(-0.5, +0.5, +0.5),
+		FVector(+0.5, +0.5, +0.5),
+	};
+
+	static TArray<FEdge> BoxEdges = 
+	{
+		{{2, 3}, {4, 1}},
+		{{2, 1}, {0, 1}},
+		{{3, 1}, {10, 1}},
+		{{0, 2}, {0, 8}},
+		{{0, 1}, {0, 6}},
+		{{4, 5}, {2, 7}},
+		{{4, 7}, {2, 3}},
+		{{5, 7}, {2, 11}},
+		{{4, 6}, {3, 9}},
+		{{4, 0}, {7, 8}},
+		{{6, 2}, {9, 4}},
+		{{6, 3}, {4, 5}},
+		{{7, 3}, {5, 10}},
+		{{1, 5}, {11, 6}},
+		{{1, 4}, {7, 6}},
+		{{0, 4}, {7, 8}},
+		{{2, 4}, {8, 9}},
+		{{7, 1}, {10, 11}},
+	};
+
 }
 
 void ARigidBodiesCustomMesh::Tick(float DeltaSeconds)
